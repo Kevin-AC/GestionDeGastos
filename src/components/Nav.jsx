f5bc53d7d1d9c4089c36d1b24feb38740686514e
Kevin-AC <kevin.argumedo@gmail.com>
2026-03-10 14:33:07 -0500

    Feat: Reorganizar estructura de carpetas y eliminar componentes obsoletos

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Login from "./Login";
+import Login from "../pages/Login";
 
 export default function Nav(){
     return(
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Login from './components/Login.jsx';
-import NewGasto from './components/NewGasto.jsx'
+import Login from './pages/Login.jsx';
+import NewGasto from './pages/NewGasto.jsx'
+import ListaGastos from './pages/ListaGastos.jsx';
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/'element={<App/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/newGasto' element={<NewGasto />} />
+      <Route path='/listaGastos' element={<ListaGastos />} />
     </Routes>
   </BrowserRouter>
   
