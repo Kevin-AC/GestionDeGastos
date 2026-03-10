86510116858eea17712e64f5a3686dfc96627f45
Kevin-AC <kevin.argumedo@gmail.com>
2026-03-10 14:03:31 -0500

    Feat:Instalacion react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import Login from './components/Login.jsx';
+import NewGasto from './components/NewGasto.jsx'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/'element={<App/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/newGasto' element={<NewGasto />} />
+    </Routes>
+  </BrowserRouter>
+  
 )
