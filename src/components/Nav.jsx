aa4f7434bc63c5a2726b7f29887354006ff8c29d
Kevin-AC <kevin.argumedo@gmail.com>
2026-03-11 21:34:11 -0500

    fix: Error, conficto de etiqueta <a> con navegacion <Link>

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,4 @@
 import { Link } from "react-router-dom";
-import Login from "../pages/Login";
-
 export default function Nav(){
     return(
         <nav className="bg-Neutral-1/90 border-b border-Neutral-2/50 shadow-lg sticky top-0 z-50">
@@ -8,14 +6,16 @@ export default function Nav(){
                 <div className="flex items-center justify-between">
                     {/* logo */}
                     <div className="bg-Verde/20 border border-Verde w-auto p-2 rounded-2xl hover:bg-Verde text-gray-900 hover:text-white ">
-                        <h1 className="text-2xl font-black   ">Gestion de Gastos</h1>
+                        <h1 className="text-2xl font-black">Gestion de Gastos</h1>
                     </div>
 
                     {/* Desktop Nav Links */}
                     <ul className="hidden md:flex items-center gap-8">
-                        <li><Link to='/'><a href="#" className="text-lg font-semibold text-gray-700 hover:text-Verde hover:underline underline-offset-4 transition-all">Home</a></Link></li>
-                        <li><Link to='/newGasto'><a href="#" className="text-lg font-semibold text-gray-700 hover:text-Verde hover:underline underline-offset-4 transition-all">Agrergar Gastos</a></Link></li>
-                        <li><Link to='/newIngreso'><a href="#" className="text-lg font-semibold text-gray-700 hover:text-Verde hover:underline underline-offset-4 transition-all">Agregar Ingresos</a></Link></li>
+                        <li><Link to='/'
+                            className="text-lg font-semibold text-gray-700 hover:text-Verde hover:underline underline-offset-4 transition-all">Home</Link></li>
+                        <li><Link to='/newGasto'
+                            className="text-lg font-semibold text-gray-700 hover:text-Verde hover:underline underline-offset-4 transition-all">Agrergar Gastos</Link></li>
+                        <li><Link to='/newIngreso' className="text-lg font-semibold text-gray-700 hover:text-Verde hover:underline underline-offset-4 transition-all">Agregar Ingresos</Link></li>
                         <li><a href="#" className="text-lg font-semibold text-gray-700 hover:text-Verde hover:underline underline-offset-4 transition-all">Estadisticas</a></li>
                     </ul>
                 </div>
