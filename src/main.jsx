9c29b60c63e4d9bff848a49816a14156a0cb8260
Kevin-AC <kevin.argumedo@gmail.com>
2026-03-13 13:40:10 -0500

    Feat:Ruta pagina para crear usuario

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from './App.jsx'
 import Login from './pages/Login.jsx';
 import NewGasto from './pages/NewGasto.jsx'
 import ListaGastos from './pages/ListaGastos.jsx';
+import RegistroUsuario from './pages/RegistroUsuario.jsx';
 import { CategoriaProvider } from './contexts/CategoriasContexts.jsx';
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')).render(
         <Route path='/login' element={<Login />} />
         <Route path='/newGasto' element={<NewGasto />} />
         <Route path='/listaGastos' element={<ListaGastos />} />
+        <Route path='/registro' element={<RegistroUsuario/>}/>
       </Routes>
    </CategoriaProvider>
   </BrowserRouter>
