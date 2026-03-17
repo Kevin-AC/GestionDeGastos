58fe3450e3530b49106fcd40753dc2990971a314
Kevin-AC <kevin.argumedo@gmail.com>
2026-03-17 00:23:24 -0500

    Feat:pasar ID de categoria por <Link> State

--- src/components/CardCategoria.jsx
+++ src/components/CardCategoria.jsx
@@ -1,9 +1,8 @@
 import { Link } from "react-router-dom";
-import { useCategory } from "../contexts/CategoriasContexts";
 
 export default function Card({ nombre,valorP,valorG,catId}) {
-    const { setCategoriaId } = useCategory();
-    console.log(setCategoriaId)
+    
+ 
     return (
         <div className="w-80 h-96 p-4 bg-Neutral-1/80 backdrop-blur-xl rounded-3xl shadow-xl border border-Neutral-2/50 hover:shadow-2xl hover:-translate-y-1 transition-all duration-300 space-y-4">
             <h2 className="text-2xl font-black text-gray-900 leading-tight">{nombre}</h2>
@@ -26,7 +25,7 @@ export default function Card({ nombre,valorP,valorG,catId}) {
                         Agregar
                     </button>
                 </Link>
-                <Link to='/listaGastos' onClick={() => setCategoriaId(catId)}>
+                <Link to='/listaGastos' state={{ categoriaId: catId }}>
                     <button className="flex-1 h-10 px-4 text-sm font-semibold text-gray-700 bg-Neutral hover:bg-Neutral/80 active:scale-95 rounded-2xl shadow-lg hover:shadow-xl transition-all duration-200 border border-Neutral-2/50">
                         Ver
                     </button>
