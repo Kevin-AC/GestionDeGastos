db9a3ebd4f6c034128238acff97f0cc62fc35013
Kevin-AC <kevin.argumedo@gmail.com>
2026-03-16 23:19:57 -0500

    Chore:correccion de nombre del Provider

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,14 @@ import Login from './pages/Login.jsx';
 import NewGasto from './pages/NewGasto.jsx'
 import ListaGastos from './pages/ListaGastos.jsx';
 import RegistroUsuario from './pages/RegistroUsuario.jsx';
-import { CategoriaProvider } from './contexts/CategoriasContexts.jsx';
+import {GastosProvider } from './contexts/GastosProvider.jsx';
 import NewIngreso from './pages/NewIngreso.jsx';
 import Estadistica from './pages/Estadistica.jsx';
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-   <CategoriaProvider>
+   <GastosProvider>
       <Routes>
-        <Route path='/' element={<App />} />
+        <Route path='/' element={<App/>} />
         <Route path='/login' element={<Login />} />
         <Route path='/newGasto' element={<NewGasto />} />
         <Route path='/listaGastos' element={<ListaGastos />} />
@@ -22,7 +22,7 @@ createRoot(document.getElementById('root')).render(
         <Route path='/newIngreso' element={<NewIngreso/>}/>
         <Route path='/estadistica' element={<Estadistica/>}/>
       </Routes>
-   </CategoriaProvider>
+    </GastosProvider>
   </BrowserRouter>
   
 )
