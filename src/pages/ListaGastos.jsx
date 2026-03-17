dcca17095417653c3d541e4061a298eb935e2b8c
Kevin-AC <kevin.argumedo@gmail.com>
2026-03-17 00:24:39 -0500

    Feat:Mostrar lista de gastos correspondientes a la categoria

--- src/pages/ListaGastos.jsx
+++ src/pages/ListaGastos.jsx
@@ -1,36 +1,39 @@
+import { useLocation } from 'react-router-dom';
+import { useContext } from 'react';
+import { GastosContext } from '../contexts/GastosProvider';
 import CardGasto from "../components/CardGasto";
 import Nav from "../components/Nav";
-import { useData } from "../hook/useData";
-import { useCategory } from "../contexts/CategoriasContexts";
-import { calcularGastoPorCategoria } from "../logic/calcularGastoPorCategoria";
-export default function ListaGastos(){
-    const { categoriaId } = useCategory();
-    const data=useData();
-    // ✅ console.log ANTES del return
-    console.log('ID:', categoriaId);
-    console.log('Data:', data);
-    const gastoTotal = calcularGastoPorCategoria(data?.[categoriaId]?.gastos || []);
 
-    if(!data)return
+export default function ListaGastos(){
+    const context = useContext(GastosContext);  // Verboso OK
+    const location = useLocation();
+    const { categoriasConGastos } = context || {};
+
+    if (!categoriasConGastos) return
+
+    const categoriaId = location.state?.categoriaId; //recibir el ID de la categoria 
+
+    const categoriaSeleccionada = categoriasConGastos?.find(categoria => categoria.id === categoriaId);
+    const gastosCategoria=categoriaSeleccionada?.gastos || [];
+    const nombreCategoria=categoriaSeleccionada?.nombre 
+
     return(
         <section>
             <Nav/>
             <div className="p-8 max-w-4xl mx-auto space-y-6">
-
-                {/* Categoría Comida */}
                 <div className="p-6 bg-linear-to-r from-gray-50 to-Neutral-1/50 backdrop-blur-sm rounded-3xl shadow-xl border border-Neutral-2/30 hover:shadow-2xl transition-all">
                     <div className="flex items-center justify-between mb-4">
                         <div className="flex items-center gap-4">
                             <div className="w-12 h-12 rounded-2xl bg-Verde/10 border-2 border-Verde/30 flex items-center justify-center">
-                                <span className="text-xl font-bold text-Verde">{data?.[categoriaId].nombre.slice(0, 1)}</span>
+                                <span className="text-xl font-bold text-Verde">{nombreCategoria.slice(0,1)}</span>
                             </div>
                             <div>
-                                <h3 className="text-xl font-black text-gray-900">{data?.[categoriaId].nombre}</h3>
+                                <h3 className="text-xl font-black text-gray-900">{nombreCategoria}</h3>
                                 <p className="text-sm text-gray-600">3 gastos</p>
                             </div>
                         </div>
                         <div className="text-right">
-                            <p className="text-2xl font-bold text-gray-900">${gastoTotal.toLocaleString()}</p>
+                            <p className="text-2xl font-bold text-gray-900">${''}</p>
                             <p className="text-sm text-gray-500">3.0% del presupuesto</p>
                         </div>
                     </div>
@@ -38,14 +41,14 @@ export default function ListaGastos(){
                         <div className="h-full bg-Verde rounded-full" style={{ width: '3%' }} />
                     </div>
                 </div>
-                {data?.[categoriaId]?.gastos?.map((gastos)=>
+                {gastosCategoria.map((gastos)=>
                     <CardGasto
                         key={gastos.id}
                         nombre={gastos.descripcion}
                         fecha={gastos.fecha}
                         valor={gastos.monto}
                     />
-                )}            
+                )}             
 
             </div>
 
