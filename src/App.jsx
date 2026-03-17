afff001f90e0ee67a4d2876ea74e11f767fa82e8
Kevin-AC <kevin.argumedo@gmail.com>
2026-03-16 23:18:22 -0500

    Feat: Rediseñar la estructura de la aplicación App.jsx pasa a se home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
 
-import Home from "./pages/Home"
-import Nav from "./components/Nav"
-
+import CardCategoria from './components/CardCategoria.jsx'
+import { useContext } from 'react';
+import { GastosContext } from './contexts/GastosProvider';
+import Nav from "./components/Nav.jsx";
 function App() {
-
+  const context = useContext(GastosContext);  // Verboso OK
+  const { categoriasConGastos, totalGeneral } = context || {};
+  if (!categoriasConGastos)return
   return (
-    <div>
+    <main>
       <Nav/>
-      <Home/>
-    </div>
+      <section className="flex">
+        <div className="w-1/3 h-screen">
+          <section className="w-3/4 h-full px-6 py-12 bg-Neutral-1/80 backdrop-blur-xl border-r border-Neutral-2/50 shadow-lg bg-linear-to-br">
+            <h1 className="text-4xl font-black text-gray-800 leading-tight mb-12">
+              {'Kevin'}
+            </h1>
+
+            <div className="space-y-1 mb-8 p-6 bg-Verde/10 backdrop-blur-md rounded-2xl border  border-Verde/30 shadow-md hover:shadow-xl transition-all duration-300 hover:-translate-y-0.5">
+              <h2 className="text-sm font-semibold text-gray-700 uppercase tracking-wider">Total</h2>
+              <p className="text-3xl font-black text-gray-900">${'150,000'}</p>
+            </div>
+
+            <div className="space-y-1 p-6  bg-Neutral/70 backdrop-blur-md rounded-2xl border border-Neutral-2/50  shadow-md hover:shadow-xl transition-all duration-300 hover:-translate-y-0.5">
+              <h2 className="text-sm font-semibold text-gray-700 uppercase tracking-wider">Total Gastos</h2>
+              <p className="text-3xl font-black text-gray-900">${totalGeneral}</p>
+            </div>
+          </section>
+        </div>
+
+        <div className="py-10 max-w-2/3 flex flex-wrap gap-5  ">
+          {categoriasConGastos.map((categoria) => (//renderizar dinamicamente cada card desde el data
+            <CardCategoria
+              key={categoria.id}
+              catId={categoria.id}
+              nombre={categoria.nombre}
+              valorP={categoria.presupuesto}
+              valorG={categoria.total}
+
+
+            />
+          ))}
+
+        </div>
+
+      </section>
+    </main>
+    
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,47 @@
-import CardCategoria from '../components/CardCategoria'
-import { useData } from '../hook/useData';
-import { calcularGastoPorCategoria } from '../logic/calcularGastoPorCategoria';
+//import CardCategoria from '../components/CardCategoria'
+//import { useGastos } from '../hook/useGastos';
 export default function Main(){
-    const data = useData();
-  
-    const calcularTotalGastos=(data)=>{//calcular el total de los gastos 
-      let total=0;
-      for(let i=1;i<data.length;i++){
-        if (data[i]&&data[i].gastos){
-          const gastosPorCategoria=calcularGastoPorCategoria(data[i].gastos);
-          //console.log(gastosPorCategoria)
-          total+=gastosPorCategoria
-        }
-      }
-      
-      return total
-    }
+    
+    
 
-    if(!data)return
     return(
+      
         <section className="flex">
                 <div className="w-1/3 h-screen">
                   <section className="w-3/4 h-full px-6 py-12 bg-Neutral-1/80 backdrop-blur-xl border-r border-Neutral-2/50 shadow-lg bg-linear-to-br">
                     <h1 className="text-4xl font-black text-gray-800 leading-tight mb-12">
-                      {data[0].nombre}
+                      {'Kevin'}
                     </h1>
         
                     <div className="space-y-1 mb-8 p-6 bg-Verde/10 backdrop-blur-md rounded-2xl border  border-Verde/30 shadow-md hover:shadow-xl transition-all duration-300 hover:-translate-y-0.5">
                       <h2 className="text-sm font-semibold text-gray-700 uppercase tracking-wider">Total</h2>
-                      <p className="text-3xl font-black text-gray-900">${data[0].saldoTotal.toLocaleString()}</p>
+                      <p className="text-3xl font-black text-gray-900">${'150,000'}</p>
                     </div>
         
                     <div className="space-y-1 p-6  bg-Neutral/70 backdrop-blur-md rounded-2xl border border-Neutral-2/50  shadow-md hover:shadow-xl transition-all duration-300 hover:-translate-y-0.5">
                       <h2 className="text-sm font-semibold text-gray-700 uppercase tracking-wider">Total Gastos</h2>
-              <p className="text-3xl font-black text-gray-900">${calcularTotalGastos(data).toLocaleString()}</p>
+              <p className="text-3xl font-black text-gray-900">${'3434'}</p>
                     </div>
-                  </section>
-        
-        
-        
-        
+                  </section>    
                 </div>
         
                 <div className="py-10 max-w-2/3 flex flex-wrap gap-5  ">
-                  {data.slice(1).map((categoria)=>(//renderizar dinamicamente cada card desde el data
+                  {/* {categoriasConGastos.map((categoria)=>(//renderizar dinamicamente cada card desde el data
                     <CardCategoria
                       key={categoria.id}
                       catId={categoria.id}
                       nombre={categoria.nombre}
-                      valorP={categoria.presupuesto.toLocaleString()}
-                      valorG={calcularGastoPorCategoria(categoria.gastos).toLocaleString()}
+                      valorP={categoria.presupuesto}
+                      valorG={categoria.total}
 
 
                     />
-                  ))}
+                  ))} */}
                   
                 </div>
         
               </section>
+              
     )
+     
 }
